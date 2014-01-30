f0dd933983e144235d02997460e97e1942311253
stuf <stefan@e21.fi>
2014-01-30 09:58:00 +0200

    Added JS solution for problem 14

---
+++ src/014-longest_collatz_sequence/014.js
@@ -0,0 +1,54 @@
+/**
+ * Project Euler - Problem 014
+ * Longest Collatz sequence
+ *
+ * The following iterative sequence is defined for the set of positive integers:
+ *   n → n/2 (n is even)
+ *   n → 3n + 1 (n is odd)
+ *
+ * Using the rule above and starting with 13, we generate the following sequence:
+ *   13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+ *
+ * It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms.
+ * Although it has not been proved yet (Collatz Problem),
+ * it is thought that all starting numbers finish at 1.
+ *
+ * Which starting number, under one million, produces the longest chain?
+ *
+ * NOTE: Once the chain starts the terms are allowed to go above one million.
+ */
+
+function findCollatzSeq(upperLimit) {
+	'use strict';
+
+	var maxChain = 0;
+	var maxChainStart = 0;
+
+	for (var i = 1; i < upperLimit; ++i) {
+		var n = i;
+		var chain = 0;
+
+		while (n != 1) {
+			if (n % 2 == 0) {
+				n = n / 2;
+			}
+			else {
+				n = 3 * n + 1;
+			}
+
+			++chain;
+		}
+
+		if (chain > maxChain) {
+			maxChain = chain;
+			maxChainStart = i;
+		}
+	}
+
+	return [maxChainStart, maxChain];
+}
+
+var timer = 'collatz:';
+console.time(timer);
+console.info(findCollatzSeq(1000000));
+console.timeEnd(timer);
